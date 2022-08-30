1c05522f7823b2c41a0b28e4a40a27cd001aa884
Fotis Papadogeorgopoulos <fotis@futurice.com>
2022-08-30 15:33:12 +0300

    Style footer

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 function init() {
   const root = ReactDOM.createRoot(
@@ -14,6 +15,8 @@ function init() {
       <App />
     </React.StrictMode>
   );
+
+  serviceWorkerRegistration.register();
 
   // If you want to start measuring performance in your app, pass a function
   // to log results (for example: reportWebVitals(console.log))

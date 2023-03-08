52c4cda0cab4f5df37722437116da9fa8771aca5
Fotis Papadogeorgopoulos <fotis@futurice.com>
2023-03-08 20:43:08 +0200

    Remove web vitals

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import * as serviceWorkerRegistration from "./serviceWorker/serviceWorkerRegistration";
 import {
   changeHtmlPreference,
@@ -32,7 +31,7 @@ function init() {
   // If you want to start measuring performance in your app, pass a function
   // to log results (for example: reportWebVitals(console.log))
   // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  reportWebVitals();
+  // reportWebVitals();
 }
 
 init();

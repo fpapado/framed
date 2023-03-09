f48b1c4d2e237e78e38199a555e209dfcb2b6ee5
Fotis Papadogeorgopoulos <fotis@futurice.com>
2023-03-09 13:42:45 +0200

    Handle hash changes when matching in tests

--- tests/service-worker.spec.ts
+++ tests/service-worker.spec.ts
@@ -1,4 +1,9 @@
 import { test, expect } from "@playwright/test";
+import {
+  HEX_CHARACTER_CLASS,
+  createRegExp,
+  removeHash,
+} from "remove-filename-hash";
 
 /**
  * Tests regarding service worker installation, in particular that the service worker exists and is installable.
@@ -23,7 +28,7 @@ test.describe("@prodOnly service worker tests", () => {
     // Now the caches can be inspected
     // (but not fetch events; those need more setup, see https://jeffy.info/2022/08/25/testing-a-service-worker.html)
     const cacheContents = await page.evaluate(async () => {
-      const cacheState = {};
+      const cacheState: Record<string, Array<string>> = {};
       for (const cacheName of await caches.keys()) {
         const cache = await caches.open(cacheName);
         const reqs = await cache.keys();
@@ -36,8 +41,53 @@ test.describe("@prodOnly service worker tests", () => {
 
     // cacheContents now contains a mapping of cache names to a
     // sorted array of URL strings contained in the cache.
-    const expectedPrecacheKey = `workbox-precache-v2-${baseURL}`;
-    expect(Object.keys(cacheContents)).toContainEqual(expectedPrecacheKey);
-    expect(cacheContents[expectedPrecacheKey]?.length).toBeGreaterThan(0);
+    // We should remove the hashed keys to be able to make stable assertions
+    // @see https://github.com/jeffposnick/yt-playlist-notifier/blob/396624c7f3471ad2bf713b87579be60572ba0c64/tests/sw.spec.ts
+    for (const [cacheName, urls] of Object.entries(cacheContents)) {
+      cacheContents[cacheName] = urls
+        .map((url) =>
+          removeHash({
+            stringWithHash: url,
+            replacement: "[hash]",
+            regexps: [
+              // Static assets hashed by vite
+              // e.g. http://localhost:4173/index-721b518b.js
+              createRegExp({
+                characters: HEX_CHARACTER_CLASS,
+                size: 8,
+                before: "-",
+                after: ".",
+              }),
+              // Hashed by workbox-build
+              // e.g. http://localhost:4173/manifest.webmanifest?__WB_REVISION__=[hash of 32 chars]
+              createRegExp({
+                characters: HEX_CHARACTER_CLASS,
+                size: 32,
+                before: "__WB_REVISION__=",
+                after: "",
+              }),
+            ],
+          })
+        )
+        .sort();
+    }
+
+    // FIXME: This can fail if we ever split the files differently (or if vite/Rollup splits them in some other way)
+    // Should we do an "at least these ones" kind of match?
+    const expected = {
+      [`workbox-precache-v2-${baseURL}`]: [
+        "/assets/CustomColorPicker-[hash].css",
+        "/assets/CustomColorPicker-[hash].js",
+        "/assets/framerFeatures-[hash].js",
+        "/assets/index-[hash].css",
+        "/assets/index-[hash].js",
+        "/assets/noto-sans-v27-latin-600-[hash].woff2",
+        "/assets/noto-sans-v27-latin-regular-[hash].woff2",
+        "/index.html?__WB_REVISION__=[hash]",
+        "/manifest.webmanifest?__WB_REVISION__=[hash]",
+      ].map((path) => new URL(path, baseURL).href),
+    };
+
+    expect(cacheContents).toEqual(expected);
   });
 });

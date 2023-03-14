eba16ab1edd1453c278d1ba8bb2aa62993202433
Fotis Papadogeorgopoulos <fotis@futurice.com>
2023-03-14 14:59:00 +0200

    Reword share test

--- tests/share-button.spec.ts
+++ tests/share-button.spec.ts
@@ -1,9 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("Share button shows up, and has correct icon", async ({
-  page,
-  browserName,
-}, testInfo) => {
+test("Share button shows up", async ({ page, browserName }, testInfo) => {
   await page.goto("/");
 
   // NOTE: Feature check must come after navigating to a page, because share is only available in secure contexts
@@ -13,13 +10,13 @@ test("Share button shows up, and has correct icon", async ({
     `This browser does not support sharing. browserName: ${browserName}, platform: ${process.platform}`
   );
 
-  await expect(page.getByRole("button", { name: "Share" })).toBeVisible();
+  const shareButton = page.getByRole("button", { name: "Share" });
+  await expect(shareButton).toBeVisible();
 
   // Exercise browser-style detection
-  const expectedShareStyle =
-    process.platform === "darwin" ? "apple" : "android";
+  const expectedShareStyleId = `share-style-${
+    process.platform === "darwin" ? "apple" : "android"
+  }`;
 
-  await expect
-    .soft(page.getByTestId(`share-style-${expectedShareStyle}`))
-    .toBeVisible();
+  await expect(page.getByTestId(expectedShareStyleId)).toBeVisible();
 });
